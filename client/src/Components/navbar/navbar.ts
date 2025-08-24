c20b66e1b2de5a2406841d2eebd3245b76341ba3
TamerGamall <tamerrgamall@gmail.com>
2025-08-24 14:44:17 +0300

    feat(navbar): add responsive navbar component

---
+++ client/src/Components/navbar/navbar.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {
+
+
+  isDark = false;
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    if (this.isDark) {
+      document.body.classList.add('dark-theme');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.body.classList.remove('dark-theme');
+      localStorage.setItem('theme', 'light');
+    }
+  }
+
+}
