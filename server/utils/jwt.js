a085651e5dd25b952a5861601b1794bfbb9ad93b
Tamer Gamal <111420806+TamerGamall@users.noreply.github.com>
2025-08-27 22:32:29 +0300

    Update jwt.js
    Token Time Update

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const generateAccessToken = (userId) => {
   return jwt.sign({ userId }, process.env.JWT_ACCESS_SECRET, {
-    expiresIn: "20s",
+    expiresIn: "15m",
   });
 };
 
